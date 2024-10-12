e0ed3dee92a4f02775da87466e8cdc895cf94697
Nandan Singh Danu <thenandansingh1999@gmail.com>
2024-10-12 22:40:40 +0530

    program updated

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # findLeapYear
 ## Introduction
 This program checks weather the year is a leap year or not
-## enter the year
+## Description 
+Enter the year and program will tell weather the year is leap year or not
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let year=1900
-if (year%4==0)
+if (year%100==0){
+    if(year%400==0)
+        console.log("Leap Year")
+    else
+    console.log("Not a Leap Year")
+}
+else if (year%4==0)
     console.log("Leap Year")
 else
 console.log("Not a Leap Year")
